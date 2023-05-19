057e085277a3c89ec50b1972b4898c32af5bb374
Евгений Пузырёв <puzyrevea@gmail.com>
2023-05-19 16:15:16 +0700

    fixed_3

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from pages import views
 from django.views.generic.base import TemplateView
 
 app_name = 'pages'
@@ -10,6 +9,3 @@ urlpatterns = [
     path('rules/', TemplateView.as_view(template_name='pages/rules.html'),
          name='rules'),
 ]
-# Действительно очень крутая штука, которая позволит "не множить сущее(view.py)
-# без необходимости". Правда удалять его пока не буду, мало ли тесты не пройдут
-# или потом понадобится
